0c9323004711f8593f53b5e1e4cca24b745c54e6
Salohiddin Usmonaliyev <saloh.usmonali@gmail.com>
2026-03-20 16:36:53 +0500

    update

--- most_common_prefix.py
+++ most_common_prefix.py
@@ -1,18 +1,16 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        prefixs = []
-        prefix = ""
-        for index in range(len(strs)):
-            if index+1 < len(strs) and strs[index][:1] == strs[index+1][:1] or len(strs) == 1:
-                if index+1 < len(strs) and strs[index][:2] == strs[index+1][:2]:
-                    prefixs.append(strs[index][:2])
-                else:
-                    prefixs.append(strs[index][:1])
+        prefix = strs[0]
         
-        if len(prefixs)>=1:    
-            return max(prefixs)
-        else:
-            return ""
+
+        for s in strs[1:]:
+            
+            while not s.startswith(prefix):
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        return prefix
+
 print(Solution().longestCommonPrefix(strs = ["flower","flow","flight"]))
 print(Solution().longestCommonPrefix(strs = ["flower","aflow","flight", "felight"]))
 print(Solution().longestCommonPrefix(strs = ["dog","racecar","car"]))
