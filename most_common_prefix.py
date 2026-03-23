f4b0b4729e0c552c9e8d75b6eebd0a3fd944a27f
Salohiddin Usmonaliyev <saloh.usmonali@gmail.com>
2026-03-23 20:16:30 +0500

    problem: most common prefix

--- most_common_prefix.py
+++ most_common_prefix.py
@@ -1,14 +1,14 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         prefix = strs[0]
-        
 
-        for s in strs[1:]:
-            
-            while not s.startswith(prefix):
-                prefix = prefix[:-1]
-                if not prefix:
-                    return ""
+        for word in strs[1:]:
+            while not word.startswith(prefix):
+                if word==prefix:
+                    prefix = word
+                else:
+                    prefix= prefix[:-1]
+        
         return prefix
 
 print(Solution().longestCommonPrefix(strs = ["flower","flow","flight"]))
